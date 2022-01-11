1e967c467fde2b57fdcfca5e96f7bc63c261e34f
youbo <stolze.luca@gmail.com>
2022-01-11 19:19:32 +0100

    MySQLMessageLater structure fix

--- src/main/java/de/brentspine/kanyujiapi/mysql/data/MySQLMessageLater.java
+++ src/main/java/de/brentspine/kanyujiapi/mysql/data/MySQLMessageLater.java
@@ -7,8 +7,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.UUID;
+
 
 public class MySQLMessageLater {
 
